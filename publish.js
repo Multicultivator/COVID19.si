d456d438bed89199d2401fb372dd5046b15e1b54
Andraz Vrhovec <andraz@koofr.net>
2020-03-17 21:58:22 +0100

    wip

--- publish.js
+++ publish.js
@@ -1,6 +1,8 @@
 const ghpages = require('gh-pages');
 
 
-ghpages.publish('dist', function (err) {
+ghpages.publish('dist', {
+  push: false
+}, function (err) {
   console.log("Publish done", err)
 });
